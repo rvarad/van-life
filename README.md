cb0d5ac341b9d4880f9f3b170d606dbf6781295b
Varad Rampurikar <rampurikarvarad@gmail.com>
2024-06-25 16:49:15 +0530

    styling

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# Vanlife
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A market-place for van rentals.
 
-Currently, two official plugins are available:
+List your vans, rent new ones.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Built using ReactJS for UI, React Router for client-side routing.
+
+# Live : [Vanlife](https://rvarad-vanlife.netlify.app/)
--- src/api.js
+++ src/api.js
@@ -1,45 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAN_pOdkiJWXqNl8VhS96ueLC0w8l3Amh0",
+	authDomain: "van-life-ae54a.firebaseapp.com",
+	projectId: "van-life-ae54a",
+	storageBucket: "van-life-ae54a.appspot.com",
+	messagingSenderId: "323776807111",
+	appId: "1:323776807111:web:901acd2df43fed84673ee2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getFirestore(app)
+
+
+
+
+///////////////////////////////////////////////
 export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+	const url = id ? `/api/vans/${id}` : "/api/vans"
+	const res = await fetch(url)
+	if (!res.ok) {
+		throw {
+			message: "Failed to fetch vans",
+			statusText: res.statusText,
+			status: res.status
+		}
+	}
+	const data = await res.json()
+	return data.vans
 }
 
 export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+	const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+	const res = await fetch(url)
+	if (!res.ok) {
+		throw {
+			message: "Failed to fetch vans",
+			statusText: res.statusText,
+			status: res.status
+		}
+	}
+	const data = await res.json()
+	return data.vans
 }
 
 
 export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
+	const res = await fetch("/api/login",
+		{ method: "post", body: JSON.stringify(creds) }
+	)
+	const data = await res.json()
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+	if (!res.ok) {
+		throw {
+			message: data.message,
+			statusText: res.statusText,
+			status: res.status
+		}
+	}
 
-    return data
+	return data
 }
